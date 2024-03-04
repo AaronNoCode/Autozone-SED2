f15965ad526c042d01221e23f95ccdc704b2fea1
Aaron <caper2711@hotmail.com>
2024-03-03 20:03:32 -0600

    Add: Class service with indexing implemented

--- indexServicio.cpp
+++ indexServicio.cpp
@@ -6,27 +6,28 @@ using namespace std;
 class IndexTuple {
 private:
     int index;
-    string id;
+    int id;
 public:
     // Constructors
     IndexTuple() = default;
-    IndexTuple(int newIndex, const string &newId) : index(newIndex), id(newId) {}
+    IndexTuple(int newIndex, int newId) : index(newIndex), id(newId) {}
 
     // Getters and setters
     [[nodiscard]] int getIndex() const { return this->index; }
-    [[nodiscard]] string getId() const { return this->id; }
+    [[nodiscard]] int getId() const { return this->id; }
     void setIndex(int newIndex) { this->index = newIndex; }
-    void setId(const string &newId) { this->id = newId; }
+    void setId(int newId) { this->id = newId; }
 
-    friend ofstream &operator<< (ofstream &ofs, const IndexTuple &idx){
+    friend ofstream &operator << (ofstream &ofs, const IndexTuple &idx){
         ofs << idx.index << '*' << idx.id << '#';
         return ofs;
     }
-    friend istream &operator>> (istream &is, IndexTuple &idx) {
+    friend istream &operator >> (istream &is, IndexTuple &idx) {
         string strAux;
         getline(is,strAux, '*');
         idx.index = stoi(strAux);
-        getline(is, idx.id, '#');
+        getline(is, strAux, '#');
+        idx.setId(stoi(strAux));
         return is;
     }
     friend class Servicio;
--- servicio.cpp
+++ servicio.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 #include "indexServicio.cpp"
 
-/* Registro de longitud variable usando indización y separadores */
+/* Registro de longitud variable usando indización */
 
 using namespace std;
 
+string servicesIndexFile = "backups/servicesIndex.txt";
+string servicesIndexFileAux = "backups/servicesIndexAux.txt";
+string servicesFile = "backups/services.txt";
+string servicesFileAux = "backups/servicesAux.txt";
+
 class Servicio {
 private:
-    string servicesIndexFile = "backups/servicesIndex.txt";
-    string servicesIndexFileAux = "backups/servicesIndexAux.txt";
-    string servicesFile = "backups/services.txt";
-    string servicesFileAux = "backups/servicesAux.txt";
-    string id;
-    string cost;
-    string name;
+    int id; // Max 9 chars
+    char cost[11]; // 10 chars (7 cypher, 2 decimals and dot)
+    char name[31]; // 30 chars ~ 49 chars (51 including nulls) 
 public:
     Servicio(){};
-    Servicio(const string &newId, const string &newCost, const string &newNombre);
+    Servicio(int newId, const string &newCost, const string &newNombre);
 
-    [[nodiscard]] string getId() const;
+    [[nodiscard]] int getId() const;
     [[nodiscard]] string getCost() const;
     [[nodiscard]] string getName() const;
     
-    void setId(const string &newId);
+    void setId(int newId);
     void setCost(const string &newCost);
     void setName(const string &newName);
 
@@ -32,52 +34,67 @@ public:
     void imprimir();
     void buscar();
     void modificar();
-    void editarCampos(Servicio &servicio, bool &changed);
+    // void editarCampos(Servicio &servicio, bool &changed);
     void eliminar();
 
     [[nodiscard]] string toString() const;
     [[nodiscard]] string toUpper(const string &initialString);
+    [[nodiscard]] bool costValidation(const string& price);
+    [[nodiscard]] bool containsSpecialCharacter(const string& str) const;
+    Servicio editarCampos(Servicio servicio);
 
     friend ofstream &operator << (ofstream &out, const Servicio &servicio) {
-        out << servicio.id << '*';
-        out << servicio.name << '*';
-        out << servicio.cost << '#';
+        out.write((char*)&servicio, sizeof(servicio));
         return out;
     }
     friend istream &operator >> (istream &in, Servicio &servicio){
-        getline(in, servicio.id,'*');
-        getline(in, servicio.name,'*');
-        getline(in, servicio.cost, '#');
+        in.read((char*)&servicio, sizeof(servicio));
         return in;
     }
 };
 
-Servicio::Servicio(const string &newId, const string &newCost, const string &newNombre) {
+Servicio::Servicio(int newId, const string &newCost, const string &newNombre) {
     this->id = newId;
-    this->cost = newCost;
-    this->name = newNombre;
+    this->setCost(newCost);
+    this->setName(newNombre);
 }
-string Servicio::getId() const {
+int Servicio::getId() const {
     return this->id;
 }
 string Servicio::getCost() const {
-    return this->cost;
+    return string(this->cost);
 }
 string Servicio::getName() const {
-    return this->name;
+    return string(this->name);
 }
-void Servicio::setId(const string &newId) {
+void Servicio::setId(int newId) {
     this->id = newId;
 }
 void Servicio::setCost(const string &newCost) {
-    this->cost = newCost;
+    int i;
+    for (i = 0; i < newCost.length(); i++) {
+        cost[i] = newCost[i];
+        if(i == 9){
+            cost[10] = '\0';
+            break;
+        }
+    }
+    cost[i] = '\0';
 }
 void Servicio::setName(const string &newName) {
-    this->name = newName;
+    int i;
+    for (i = 0; i < newName.length(); i++) {
+        name[i] = newName[i];
+        if(i == 29){
+            name[30] = '\0';
+            break;
+        }
+    }
+    name[i] = '\0';
 }
 
 [[nodiscard]] string Servicio::toString() const {
-    return "ID: " + this->id + "\nNombre: " + this->name + "\nCosto: " + this->cost + '\n';
+    return "ID: " + to_string(this->id) + "\nNombre: " + this->getName() + "\nCosto: " + this->getCost() + '\n';
 }
 [[nodiscard]] string Servicio::toUpper(const string &initialString){
     string finalString;
@@ -86,23 +103,65 @@ void Servicio::setName(const string &newName) {
     }
     return finalString;
 }
+bool Servicio::costValidation(const string& price){
+    int priceLength = price.length();
+    if(priceLength > 10){
+        cout << "Numero demasiado grande" << endl;
+        return false;
+    }
+    for(int i = 0; i < priceLength-1; i++){
+        if(!isdigit(price[i]) && price[i] != '.'){
+            cout << "Formato de numero incorrecto" << endl; 
+            return false;
+        }
+        if(price[i] == '.' && price[i+3] != '\0'){
+            cout << "El precio solo debe tener dos decimales" << endl;
+            return false;
+        }
+    }
+    return true;
+}
+bool Servicio::containsSpecialCharacter(const string& str) const {
+    for (char c : str) {
+        if (!isalnum(c) && c != ' ') {
+            return true;
+        }
+    }
+    return false;
+}
+bool containsOnlyNumber(const string &str){
+    for(char c : str){
+        if(!isdigit(c)){
+            return false;
+        }
+    }
+    return true;
+}
 void Servicio::agregar() {
     IndexTuple index;
     Servicio servicio;
     string strAux;
-    cout << "ID: ";
-    getline(cin, strAux, '\n');
-    servicio.setId(toUpper(strAux));
-    cout << "Nombre: ";
-    getline(cin, servicio.name, '\n');
-    cout << "Costo: ";
-    getline(cin, servicio.cost, '\n');
+    do{
+        cout << "ID: ";
+        getline(cin, strAux, '\n');
+    }while(containsSpecialCharacter(strAux) || !containsOnlyNumber(strAux));
+    servicio.setId(stoi(strAux));
+    do{
+        cout << "Nombre: ";
+        getline(cin, strAux, '\n');
+    } while (containsSpecialCharacter(strAux) || strAux.length() > 30 || strAux.length() == 0);
+    servicio.setName(strAux);
+    do{
+        cout << "Costo: ";
+        getline(cin, strAux, '\n');
+    } while (!costValidation(strAux) || strAux.length() == 0);
+    servicio.setCost(strAux);
 
-    ofstream outputServicesFile (servicesFile, ios::in | ios::app | ios::ate);
+    ofstream outputServicesFile (servicesFile, ios::ate | ios::app);
     ofstream outputIndexFile (servicesIndexFile, ios::app);
     
     index.setIndex(outputServicesFile.tellp());
-    index.setId(servicio.id);
+    index.setId(servicio.getId());
     outputIndexFile << index;
     outputIndexFile.close();
 
@@ -112,7 +171,7 @@ void Servicio::agregar() {
 void Servicio::imprimir() {
     ifstream inputServicesFile (servicesFile, ios::in);
     if(!inputServicesFile.good()){
-        cout << "Archivo dde servicios inexistente" << endl;
+        cout << "Archivo de servicios inexistente" << endl;
         return; 
     }
     Servicio servicio;
@@ -122,15 +181,195 @@ void Servicio::imprimir() {
     }
     inputServicesFile.close();
 }
-void Servicio::buscar() {}
-void Servicio::modificar() {}
-void Servicio::eliminar() {}
+void Servicio::buscar() {
+    ifstream inputIndexFile (servicesIndexFile, ios::in);
+    ifstream inputServicesFile (servicesFile, ios::in);
+    if(!inputIndexFile.good()){
+        cout<<"Archivo de índices de servicios no existente"<<endl;
+        return;
+    }
+    if(!inputServicesFile.good()){
+        cout<<"Archivo de servicios no existente"<<endl;
+        return;
+    }
+
+    Servicio servicio;
+    IndexTuple index;
+    string idBuscado;
+    do{
+        cout << "Ingresa ID de servicio:";
+        getline(cin, idBuscado,'\n');
+    } while(!containsOnlyNumber(idBuscado) || idBuscado.length() == 0);
+    
+    while(inputIndexFile.peek() != -1){
+        inputIndexFile >> index;
+        if(index.getId() == stoi(idBuscado)){
+            inputServicesFile.seekg(index.getIndex());
+            inputServicesFile >> servicio;
+            cout << servicio.toString();
+            inputIndexFile.close();
+            inputServicesFile.close();
+            return;
+        }
+    }
+    cout << "Servicio no encontrado" << endl; 
+    inputIndexFile.close();
+    inputServicesFile.close();
+}
+Servicio Servicio::editarCampos(Servicio servicio){
+    string strAux;
+    int option = -1;
+    while(option != 0){
+        cout << "1. ID\n2. Nombre\n3. Costo\n0. Salir\n";
+        cout << "Seleccione elemento para editar: ";
+        cin >> option;
+        cin.ignore();
+
+        switch (option) {
+        case 1:
+            do{
+                cout << "Ingresa nuevo ID de producto: ";
+                getline(cin, strAux, '\n');
+            }while(containsSpecialCharacter(strAux) || !containsOnlyNumber(strAux));
+            servicio.setId(stoi(strAux));
+            break;
+        case 2:
+            do{
+                cout << "Nombre: ";
+                getline(cin, strAux, '\n');
+            } while (containsSpecialCharacter(strAux) || strAux.length() > 30 || strAux.length() == 0);
+            servicio.setName(strAux);
+            break;
+        case 3:
+            do{
+                cout << "Costo: ";
+                getline(cin, strAux, '\n');
+            } while (!costValidation(strAux) || strAux.length() == 0);
+            servicio.setCost(strAux);
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Opción inválida" << endl;
+            break;
+        }
+    }
+    return servicio;
+}
+void Servicio::modificar() {
+    ifstream inputServicesFile(servicesFile, ios::in);
+    ofstream outputServicesFile(servicesFile, ios::in | ios::ate);
+    ifstream inputServicesIndexFile(servicesIndexFile, ios::in);
+    ofstream outputServicesIndexFile(servicesIndexFileAux, ios::out);
+    if(!inputServicesFile.good()){
+        cout<<"Archivo de servicios no existente"<<endl;
+        return;
+    }
+    if(!inputServicesIndexFile.good()){
+        cout<<"Archivo de indices de servicios no existente"<<endl;
+        return;
+    }
+
+    Servicio servicio;
+    IndexTuple index;
+    string idBuscado;
+
+    cout << "Ingrese ID del servicio a modificar: "; 
+    getline(cin, idBuscado, '\n');
+    while(inputServicesIndexFile.peek() != -1){
+        inputServicesIndexFile >> index;
+        if(index.getId() == stoi(idBuscado)){
+            inputServicesFile.seekg(index.getIndex());
+            inputServicesFile >> servicio;
+            cout << "Servicio encontrado:" << endl;
+            cout << servicio.toString() << endl;
+            servicio = editarCampos(servicio);
+            outputServicesFile.seekp(index.getIndex());
+            outputServicesFile << servicio;
+            index.setId(servicio.getId());
+        }
+        outputServicesIndexFile << index;
+    }
+     
+    inputServicesFile.close();
+    inputServicesIndexFile.close();
+    outputServicesIndexFile.close();
+
+    remove(servicesIndexFile.c_str());
+    rename(servicesIndexFileAux.c_str(), servicesIndexFile.c_str());
+}
+void Servicio::eliminar() {
+    ifstream inputIndexFile (servicesIndexFile, ios::in);
+    ifstream inputServicesFile (servicesFile, ios::in);
+    ofstream outputIndexFile (servicesIndexFileAux, ios::out);
+    ofstream outputServicesFile (servicesFileAux, ios::out);
+
+    if(!inputIndexFile.good()){
+        cout<<"Archivo de índices de servicios no existente"<<endl;
+        return;
+    }
+    if(!inputServicesFile.good()){
+        cout<<"Archivo de servicios no existente"<<endl;
+        return;
+    }
+    Servicio servicio;
+    IndexTuple index;
+    string idBuscado = " ";
+    int idRecordToOmit = 0, previousRecordIndex = 0, actualRecordIndex = 0;
+    bool changeIndex = false; 
+    do{
+        cout << "Ingresa ID de servicio a eliminar: ";
+        getline(cin, idBuscado,'\n');
+    } while(!containsOnlyNumber(idBuscado) || idBuscado.length() == 0);
+    idRecordToOmit = stoi(idBuscado);
+
+    while(inputIndexFile.peek() != -1){
+        inputIndexFile >> index;
+        if(index.getId() == idRecordToOmit) {
+            inputIndexFile.seekg(0);
+            while(inputServicesFile.peek() != -1){
+                inputIndexFile >> index;
+                inputServicesFile >> servicio;
+                if(idRecordToOmit == index.getId()){
+                    previousRecordIndex = index.getIndex();
+                    changeIndex = true;
+                    continue;
+                }
+                if(changeIndex){
+                    actualRecordIndex = index.getIndex();
+                    index.setIndex(previousRecordIndex);
+                    previousRecordIndex = actualRecordIndex;
+                    outputIndexFile << index;
+                    outputServicesFile << servicio;
+                }else{
+                    outputIndexFile << index;
+                    outputServicesFile << servicio;
+                }
+            }
+
+            inputIndexFile.close();
+            inputServicesFile.close();
+            outputIndexFile.close();
+            outputServicesFile.close();
+
+            remove(servicesFile.c_str());
+            remove(servicesIndexFile.c_str());
+            rename(servicesFileAux.c_str(), servicesFile.c_str());
+            rename(servicesIndexFileAux.c_str(), servicesIndexFile.c_str());
+            
+            return;
+        }
+    }
+    cout << "Servicio inexistente" << endl;
+}
+
 
 int main (){
     Servicio servicio;
     int option;
-    cout<<"1. Agregar\n2. Imprimir\n3. Buscar\n4. Modificar\n5. Eliminar\n6. Salir\n";
     while(option != 6){
+        cout<<"Gestion de Servicios\n====================\n";
+        cout<<"1. Agregar\n2. Imprimir\n3. Buscar\n4. Modificar\n5. Eliminar\n6. Salir\n";
         cout<<"\nOpcion: ";
         cin>>option;
         cin.ignore();
@@ -141,15 +380,15 @@ int main (){
             case 2:
                 servicio.imprimir();
                 break;
-            /* case 3:
+            case 3:
                 servicio.buscar();
                 break;
             case 4:
                 servicio.modificar();
                 break;
             case 5:
-                servicio.eliminar();
-                break; */
+                servicio.eliminar(); // TODO
+                break; 
             case 6: // Salir
                 break;
             default:
